a2f5fa9f14b12235f5784c2b637ca3d2155b1c58
Elsis-Sitati <sitatielsis@gmail.com>
2016-08-25 23:10:27 +0300

    (Feature) Make the ability to display zeros
    Display zeros according to the number of rows or columns
    declared
    closes issue #5

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
             break
 
         # if an equal sign is in text then is making an assignment
-        
+
         elif "=" in text:
             container = text.split('=')
             # putting data to variables dictionary
@@ -29,7 +29,22 @@ def main():
         elif "\'" in text:
             print variables[text[0]].transpose_matrix()
 
+        elif "+" in text:
+            container = text.split("+")
+            print variables[container[0].strip()].add_matrix(int(container[1].strip()))
 
+        elif "zeros" in text:
+            # find value for column in string and convert it to an integer
+            columns = int(text[-2])
+            # find value for row in a string and convert it to an integer
+            rows = int(text[-4])
+            # loop through rows
+            for row in range(rows):
+                # loop through the column
+                for col in range(columns):
+                    print "0 \t"
+                # outer loop
+                print "\n"
 
 
 if __name__ == '__main__':
