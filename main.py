af74d60ef0ffa6120f23874216461966dac23149
Elsis-Sitati <sitatielsis@gmail.com>
2016-08-25 14:14:26 +0300

    (Feature) Create entry point for application

---
+++ main.py
@@ -0,0 +1,30 @@
+from __future__ import unicode_literals
+from prompt_toolkit import prompt
+from prompt_toolkit.history import InMemoryHistory
+from matrix_operations import Matrix
+
+variables = {}
+
+
+def main():
+    history = InMemoryHistory()
+
+    while True:
+        text = prompt("> ", history=history)
+        text = text.strip()
+        if text == 'exit':
+            print('GoodBye!')
+            break
+
+        # if an equal sign is in text then is making an assignment
+        
+        elif "=" in text:
+            container = text.split('=')
+            # putting data to variables dictionary
+            variables[container[0].strip()] = Matrix(container[1].strip())
+
+        elif text in variables.keys():
+            print variables[text].data
+
+if __name__ == '__main__':
+    main()
